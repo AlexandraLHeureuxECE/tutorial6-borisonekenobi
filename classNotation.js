52b980e2e7b7c1746dc0a261ed7e89e75a797bd5
Boris Vasilev <bvasilev@uwo.ca>
2023-11-15 17:30:22 -0500

    Tutorial 6 tests work

--- classNotation.js
+++ classNotation.js
@@ -1,18 +1,60 @@
+// Define the Monster and Attack using the class notation
+class Monster {
+    constructor(name, strength, attacks) {
+        this.name = name
+        this.strength = strength
+        this.attacks = attacks
+    }
 
-/* Define the Monster and Attack using the class notation*/
+    monsterAttack() {
+        console.log('The ' + this.name + ' monster attacks >>> ');
+        let totalAttack = 0;
+        for(let a=0; a < this.attacks.length; a++) {
+            totalAttack += this.attacks[a].dealDamage();
+        }
+        return totalAttack;
+    };
 
-//create the objects using the class
-let attack1 
-let attack2 
-let attack3 
-let attack4 
+    getStrength() {
+        console.log('The ' + this.name + ' monster\'s strength is ' + this.strength);
+        return this.strength
+    };
+    
+    getDamaged(damage) {
+        this.strength -= damage;
+        return this.strength
+    };
+}
 
-let monster1 
-let monster2 
+class Attack {
+    constructor(name, damage) {
+        this.name = name
+        this.damage = damage
+    }
 
-//Create the calls to generate the output 
+    dealDamage() {
+        console.log('The ' + this.name + ' attack dealt ' + this.damage + ' damage');
+        return this.damage
+    };
+}
 
+// Create the objects using the class
+let attack1 = new Attack('flying', 30);
+let attack2 = new Attack('surprise', 25);
+let attack3 = new Attack('sneak', 35);
+let attack4 = new Attack('boss', 40);
 
-//DO NOT TOUCH
+let monster1 = new Monster('Fairy', 300, [attack1, attack2]);
+let monster2 = new Monster('Werewolf', 1000, [attack3, attack4]);
+
+// Create the calls to generate the output 
+Monster.prototype.getStrength.call(monster1);
+Monster.prototype.getDamaged.call(monster1,monster2.monsterAttack());
+Monster.prototype.getStrength.call(monster1);
+Monster.prototype.getStrength.call(monster2);
+Monster.prototype.getDamaged.call(monster2,monster1.monsterAttack());
+Monster.prototype.getStrength.call(monster2);
+
+// DO NOT TOUCH
 module.exports.MonsterClass=Monster
 module.exports.AttackClass=Attack
--- constructorFunctions.js
+++ constructorFunctions.js
@@ -1,24 +1,58 @@
+// Define Monster and Attack using constructor functions
+function Monster(name, strength, attacks) {
+    this.name = name;
+    this.strength = strength;
+    this.attacks = attacks;
+}
+
+function Attack(name, damage) {
+    this.name = name;
+    this.damage = damage;
+}
+
+// Add the getStrength and getDamaged functions to the Monster prototype
+Monster.prototype.monsterAttack = function() {
+    console.log('The ' + this.name + ' monster attacks >>> ');
+    let totalAttack = 0;
+    for(let a=0; a < this.attacks.length; a++) {
+        totalAttack += this.attacks[a].dealDamage();
+    }
+    return totalAttack;
+};
+
+Attack.prototype.dealDamage = function() {
+    console.log('The ' + this.name + ' attack dealt ' + this.damage + ' damage');
+    return this.damage
+};
+
+Monster.prototype.getStrength = function() {
+    console.log('The ' + this.name + ' monster\'s strength is ' + this.strength);
+    return this.strength
+};
+
+Monster.prototype.getDamaged = function(damage) {
+    this.strength -= damage;
+    return this.strength
+};
 
 
-/* Define Monster and Attack using constructor functions*/
+// Create the objects using the class
+let attack1 = new Attack('flying', 30);
+let attack2 = new Attack('surprise', 25);
+let attack3 = new Attack('sneak', 35);
+let attack4 = new Attack('boss', 40);
 
+let monster1 = new Monster('Fairy', 300, [attack1, attack2]);
+let monster2 = new Monster('Werewolf', 1000, [attack3, attack4]);
 
-//Add the getStrength and getDamaged functions to the Monster prototype
+// Create the calls to generate the output 
+Monster.prototype.getStrength.call(monster1);
+Monster.prototype.getDamaged.call(monster1, monster2.monsterAttack());
+Monster.prototype.getStrength.call(monster1);
+Monster.prototype.getStrength.call(monster2);
+Monster.prototype.getDamaged.call(monster2, monster1.monsterAttack());
+Monster.prototype.getStrength.call(monster2);
 
-
-
-//create the objects using the class
-let attack1 
-let attack2 
-let attack3 
-let attack4 
-
-let monster1 
-let monster2 
-
-//Create the calls to generate the output 
-
-
-//DO NOT TOUCH
+// DO NOT TOUCH
 module.exports.MonsterConstructor=Monster
 module.exports.AttackConstructor=Attack
